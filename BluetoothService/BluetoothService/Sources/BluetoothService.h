8c69f01f1446b1b41f77103fbaa11a155215dfe1
nenhall <nenhall@126.com>
2020-06-12 00:42:24 +0800

    完善中心设备

---
+++ BluetoothService/BluetoothService/Sources/BluetoothService.h
@@ -0,0 +1,19 @@
+//
+//  BluetoothService.h
+//  BluetoothService
+//
+//  Created by nenhall on 2020/5/30.
+//  Copyright © 2020 nenhall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BluetoothService.
+FOUNDATION_EXPORT double BluetoothServiceVersionNumber;
+
+//! Project version string for BluetoothService.
+FOUNDATION_EXPORT const unsigned char BluetoothServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BluetoothService/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+workspace 'BluetoothService.xcworkspace'
+
+target 'BluetoothDemo' do
+  use_frameworks!
+  platform :ios, '10.0'
+  project 'BluetoothDemo/BluetoothDemo'
+
+  pod 'SnapKit'
+  pod 'Toast-Swift'
+  pod 'IBAnimatable'
+  pod 'IQKeyboardManagerSwift'
+
+end
